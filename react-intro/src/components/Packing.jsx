a29a2ce5ef3fcd6f0b725dc6ed3560620da2ca1c
M-Talha183 <ullahehsan016@gmail.com>
2026-08-12 14:09:24 +0500

    Add usestate conditional rendering

--- react-intro/src/components/Packing.jsx
+++ react-intro/src/components/Packing.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Item from './Item'
+import {useState} from 'react'
 
 function Packing() {
+
+  const [isPacked, setIsPacked] = useState(false)
+
+  function changeStatus() {
+    if (isPacked) {
+      setIsPacked(false)
+    } else {
+      setIsPacked(true)
+    }
+  }
   return (
-    <div className="w-full h-48 flex flex-col items-center justify-center gap-4 bg-gray-100">
+    <div className="w-full h-96 flex flex-col items-center justify-center gap-4 bg-gray-100">
       <h1 className="text-2xl font-bold">This is the packing component</h1>
-      <Item name="Item 1" isPacked={true} />
-      <Item name="Item 2" isPacked={false} />
-      <Item name="Item 3" isPacked={true} />
+      <Item name="Item 1" isPacked={isPacked}  /> <button className='w-48 h-14 border-2 text-2xl text-white border-gray-300 rounded-xl bg-amber-800 mb-10 p-10' onClick={changeStatus}>Toggle</button>
+      <Item name="Item 2" isPacked={isPacked} /> <button className='w-48 h-14 border-2 text-2xl text-white border-gray-300 rounded-xl bg-amber-800 mb-10 p-10' onClick={changeStatus}>Toggle</button>
+      <Item name="Item 3" isPacked={isPacked} /> <button className='w-48 h-14 border-2 text-2xl text-white border-gray-300 rounded-xl bg-amber-800 mb-10 p-10' onClick={changeStatus}>Toggle</button>
+
+      
 
     </div>
   )
