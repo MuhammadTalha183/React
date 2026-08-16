e94b1d3151af24b8b902bbf85600af4b2cfe6705
M-Talha183 <ullahehsan016@gmail.com>
2026-08-17 03:01:07 +0500

    start working on assignment

--- advice-app/src/components/advice.jsx
+++ advice-app/src/components/advice.jsx
@@ -5,20 +5,20 @@ let advives = ["Learn HTML" , "Learn CSS", "Learn Javascript "]
 function Advice () {
     let [ step, setStep ] = useState(0)
 
-    function handleNext () {
-       step < advives.length - 1 ? setStep(step + 1 ) : setStep(0)
-    }
-    function handlePrevious (){
-       step == 0 ? setStep(advives.length - 1) : setStep( step - 1)
-    }
+    // function handleNext () {
+    //    step < advives.length - 1 ? setStep(step + 1 ) : setStep(0)
+    // }
+    // function handlePrevious (){
+    //    step == 0 ? setStep(advives.length - 1) : setStep( step - 1)
+    // }
     return (
         <>
         <div></div>
         <div>Advice :
             {advives[step]} </div>
         <div>
-            <button onClick={handleNext}>Next</button>
-            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={()=>{step < advives.length - 1 ? setStep(step + 1 ) : setStep(0)}}>Next</button>
+            <button onClick={()=>{step == 0 ? setStep(advives.length - 1) : setStep( step - 1)}}>Previous</button>
         </div>
         </>
     )
